95314964219506fe492f69199256982d1d610663
affansaiyed <affanimam123@gmail.com>
2023-11-23 21:36:25 -0500

    final copy of script

--- tables.sql
+++ tables.sql
@@ -1,68 +1,93 @@
 CREATE TABLE Student (
-    Student_ID INT PRIMARY KEY,
-    First_Name VARCHAR(50),
-    Last_Name VARCHAR(50),
-    Gender VARCHAR(10),
-    Pronounce VARCHAR(50),
-    International_Student BOOLEAN,
-    Date_of_Admission DATE,
-    Overall_GPA DECIMAL(3, 2)
+    Student_ID INT PRIMARY KEY NOT NULL,
+    First_Name VARCHAR(50) NOT NULL,
+    Last_Name VARCHAR(50) NOT NULL,
+    Gender VARCHAR(20) NOT NULL,
+    Pronouns VARCHAR(20) NOT NULL,
+    International_Student BOOLEAN NOT NULL,
+    Date_of_Admission DATE NOT NULL,
+    Overall_GPA DECIMAL(3, 2) CHECK (Overall_GPA >= 0)
 );
 
 CREATE TABLE Course (
-    Course_Code VARCHAR(10) PRIMARY KEY,
-    Semester VARCHAR(20),
-    Instructor_Name VARCHAR(50),
+    Course_Code VARCHAR(15) NOT NULL,
+    Course_Name VARCHAR(50) NOT NULL,
+    Semester VARCHAR(10) NOT NULL,
+    Instructor_Name VARCHAR(50) NOT NULL,
     Midterm_1_Grade DECIMAL(3, 2),
     Midterm_2_Grade DECIMAL(3, 2),
     Project_Grade DECIMAL(3, 2),
-    Final_Exam_Grade DECIMAL(3, 2),
+    Final_Exam_Grade DECIMAL(3, 2) NOT NULL,
     Final_Grade DECIMAL(3, 2),
-    Completion_Status VARCHAR(20)
+    Completion_Status BOOLEAN NOT NULL,
+    FOREIGN KEY (Course_Code) REFERENCES Course(Course_Code) NOT NULL
 );
 
 CREATE TABLE Registration (
-    Student_ID INT,
-    Course_Code VARCHAR(10),
-    Registration_Date DATE,
-    PRIMARY KEY (Student_ID, Course_Code),
-    FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID),
-    FOREIGN KEY (Course_Code) REFERENCES Course(Course_Code)
+    Student_ID INT NOT NULL,
+    Course_Code VARCHAR(15) PRIMARY KEY NOT NULL,
+    Registration_Date DATE NOT NULL,
+    FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID) NOT NULL
 );
+
 CREATE TABLE Location (
-    Address VARCHAR(100),
-    City VARCHAR(50),
-    Province VARCHAR(50),
-    Country VARCHAR(50),
-    Postal_Code VARCHAR(10),
-    Student_ID INT,
-    PRIMARY KEY (Student_ID),
-    FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID)
+    Address VARCHAR(100) NOT NULL,
+    City VARCHAR(50) NOT NULL,
+    Province VARCHAR(50) NOT NULL,
+    Country VARCHAR(50) NOT NULL,
+    Postal_Code VARCHAR(10) NOT NULL,
+    Student_ID INT NOT NULL,
+    FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID) NOT NULL
 );
+
 CREATE TABLE Contact_Information (
     Student_Email VARCHAR(100),
-    Phone_Number VARCHAR(15),
-    Emergency_Contact_1 VARCHAR(100),
-    Emergency_Contact_2 VARCHAR(100),
-    Student_ID INT,
-    PRIMARY KEY (Student_ID),
-    FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID)
+    Student_Email_2 VARCHAR(50) NOT NULL,
+    Phone_Number VARCHAR(20),
+    School_Name_1 VARCHAR(50) NOT NULL,
+    School_Name_2 VARCHAR(50) NOT NULL,
+    Emergency_Contact_Name_1 VARCHAR(50) NOT NULL,
+    Emergency_Contact_Number_1 VARCHAR(15) NOT NULL,
+    Emergency_Contact_Email_1 VARCHAR(50) NOT NULL,
+    Emergency_Contact_Name_2 VARCHAR(50) NOT NULL,
+    Emergency_Contact_Number_2 VARCHAR(15) NOT NULL,
+    Emergency_Contact_Email_2 VARCHAR(50) NOT NULL,
+    Student_ID INT NOT NULL,
+    FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID) NOT NULL
 );
+
 CREATE TABLE Extracurricular (
-    Club_Name VARCHAR(50),
-    Job_Name VARCHAR(50),
-    Student_ID INT,
-    PRIMARY KEY (Student_ID),
-    FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID)
+    Club_ID VARCHAR(50) NOT NULL,
+    Job_ID VARCHAR(50) NOT NULL,
+    Student_ID INT NOT NULL,
+    FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID) NOT NULL
 );
+
+CREATE TABLE Jobs (
+    Job_ID INT NOT NULL,
+    Job_Name VARCHAR(50) NOT NULL,
+    FOREIGN KEY (Job_ID) REFERENCES Extracurricular(Job_ID) NOT NULL
+);
+
+CREATE TABLE Clubs (
+    Club_ID INT NOT NULL,
+    Club_Name VARCHAR(50) NOT NULL,
+    FOREIGN KEY (Club_ID) REFERENCES Extracurricular(Club_ID) NOT NULL
+);
+
 CREATE TABLE Graduate (
-    Undergraduate_GPA DECIMAL(3, 2),
-    Undergraduate_Degree VARCHAR(50),
-    Admission_Average DECIMAL(3, 2),
-    Program_Major VARCHAR(50),
-    Program_Minor VARCHAR(50),
-    Double_Degree BOOLEAN,
-    Student_ID INT,
-    PRIMARY KEY (Student_ID),
-    FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID)
+    Undergraduate_GPA DECIMAL(3, 2) CHECK (Undergraduate_GPA >= 0),
+    Undergraduate_Degree VARCHAR(50) NOT NULL,
+    Program_Minor VARCHAR(50) NOT NULL,
+    Double_Degree BOOLEAN NOT NULL,
+    Student_ID INT NOT NULL,
+    FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID) NOT NULL
+);
+
+CREATE TABLE Second_Major (
+    Student_ID INT NOT NULL,
+    Student_ID_2 INT NOT NULL,
+    School_Name_2 VARCHAR(50) NOT NULL,
+    Student_Email_2 VARCHAR(50) NOT NULL,
+    FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID),
 );
--- test_suite.sql
+++ test_suite.sql
@@ -5,4 +5,36 @@ SELECT * FROM Student WHERE Student_ID = 101;
 UPDATE Student SET Overall_GPA = 3.9 WHERE Student_ID = 101;
 DELETE FROM Course WHERE Course_Code = 'CS101';
 INSERT INTO Location (Address, City, Province, Country, Postal_Code, Student_ID) VALUES ('123 Main St', 'Waterloo', 'Ontario', 'Canada', 'N2L 3C5', 101);
-INSERT INTO Student (Student_ID, First_Name, Last_Name, Gender, Date_of_Admission, Overall_GPA) VALUES (102, 'Bob', 'Johnson', 'Male', '2022-01-20', 'High');
+
+INSERT INTO Student (Student_ID, First_Name, Last_Name, Gender, Date_of_Admission, Overall_GPA) VALUES (001, 'Bob', 'Johnson', 'Male', '2022-01-20', 'High');
+INSERT INTO Student (Student_ID, First_Name, Last_Name, Gender, Date_of_Admission, Overall_GPA) VALUES (002, 'Alice', 'Smith', 'Female', '2022-01-15', 3.8);
+INSERT INTO Student (Student_ID, First_Name, Last_Name, Gender, Date_of_Admission, Overall_GPA) VALUES (003, 'Dani', 'Harvey', 'Male', '2022-01-15', 3.6);
+INSERT INTO Student (Student_ID, First_Name, Last_Name, Gender, Date_of_Admission, Overall_GPA) VALUES (004, 'Charlie', 'Smith', 'Male', '2022-01-15', 3.4);
+INSERT INTO Student (Student_ID, First_Name, Last_Name, Gender, Date_of_Admission, Overall_GPA) VALUES (005, 'Delta', 'Thorne', 'Male', '2022-01-15', 3.9);
+INSERT INTO Student (Student_ID, First_Name, Last_Name, Gender, Date_of_Admission, Overall_GPA) VALUES (006, 'Edward', 'Jackson', 'Male', '2022-01-15', 3.1);
+INSERT INTO Student (Student_ID, First_Name, Last_Name, Gender, Date_of_Admission, Overall_GPA) VALUES (007, 'Riley', 'Weathers', 'Female', '2022-01-15', 4.0);
+INSERT INTO Student (Student_ID, First_Name, Last_Name, Gender, Date_of_Admission, Overall_GPA) VALUES (008, 'John', 'Doe', 'Male', '2022-01-15', 3.5);
+INSERT INTO Student (Student_ID, First_Name, Last_Name, Gender, Date_of_Admission, Overall_GPA) VALUES (009, 'Jane', 'Doe', 'Female', '2022-01-15', 3.7);
+INSERT INTO Student (Student_ID, First_Name, Last_Name, Gender, Date_of_Admission, Overall_GPA) VALUES (000, 'Clifford', 'Rock', 'Male', '2022-01-15', 3.1);
+
+INSERT INTO Contact_Information (Student_Email, Phone_Number, Emergency_Contact_1, Emergency_Contact_2, Student_ID) VALUES (bob@mylaurier.ca, 1, 2, 3, 001)
+INSERT INTO Contact_Information (Student_Email, Phone_Number, Emergency_Contact_1, Emergency_Contact_2, Student_ID) VALUES (ali@mylaurier.ca, 2, 3, 4, 002)
+INSERT INTO Contact_Information (Student_Email, Phone_Number, Emergency_Contact_1, Emergency_Contact_2, Student_ID) VALUES (dan@mylaurier.ca, 3, 4, 5, 003)
+INSERT INTO Contact_Information (Student_Email, Phone_Number, Emergency_Contact_1, Emergency_Contact_2, Student_ID) VALUES (cha@mylaurier.ca, 4, 5, 6, 004)
+INSERT INTO Contact_Information (Student_Email, Phone_Number, Emergency_Contact_1, Emergency_Contact_2, Student_ID) VALUES (del@mylaurier.ca, 5, 6, 7, 005)
+INSERT INTO Contact_Information (Student_Email, Phone_Number, Emergency_Contact_1, Emergency_Contact_2, Student_ID) VALUES (edw@mylaurier.ca, 6, 7, 8, 006)
+INSERT INTO Contact_Information (Student_Email, Phone_Number, Emergency_Contact_1, Emergency_Contact_2, Student_ID) VALUES (ril@mylaurier.ca, 7, 8, 9, 007)
+INSERT INTO Contact_Information (Student_Email, Phone_Number, Emergency_Contact_1, Emergency_Contact_2, Student_ID) VALUES (joh@mylaurier.ca, 8, 9, 0, 008)
+INSERT INTO Contact_Information (Student_Email, Phone_Number, Emergency_Contact_1, Emergency_Contact_2, Student_ID) VALUES (jan@mylaurier.ca, 9, 1, 1, 009)
+INSERT INTO Contact_Information (Student_Email, Phone_Number, Emergency_Contact_1, Emergency_Contact_2, Student_ID) VALUES (cli@mylaurier.ca, 0, 0, 2, 000)
+
+INSERT INTO Registration (Student_ID, Course_Code, Registration_Date) VALUES (001, 'CS101', '2022-09-01');
+INSERT INTO Registration (Student_ID, Course_Code, Registration_Date) VALUES (002, 'CS101', '2022-09-01');
+INSERT INTO Registration (Student_ID, Course_Code, Registration_Date) VALUES (003, 'CS101', '2022-09-01');
+INSERT INTO Registration (Student_ID, Course_Code, Registration_Date) VALUES (004, 'CS101', '2022-09-01');
+INSERT INTO Registration (Student_ID, Course_Code, Registration_Date) VALUES (005, 'CS101', '2022-09-01');
+INSERT INTO Registration (Student_ID, Course_Code, Registration_Date) VALUES (006, 'CS101', '2022-09-01');
+INSERT INTO Registration (Student_ID, Course_Code, Registration_Date) VALUES (007, 'CS101', '2022-09-01');
+INSERT INTO Registration (Student_ID, Course_Code, Registration_Date) VALUES (008, 'CS101', '2022-09-01');
+INSERT INTO Registration (Student_ID, Course_Code, Registration_Date) VALUES (009, 'CS101', '2022-09-01');
+INSERT INTO Registration (Student_ID, Course_Code, Registration_Date) VALUES (000, 'CS101', '2022-09-01');
