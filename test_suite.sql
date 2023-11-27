aa2938adc0b0cf291adc45c4a2db7cc20825da92
affansaiyed <affanimam123@gmail.com>
2023-11-27 01:02:48 -0500

    test suite done

--- test_suite.sql
+++ test_suite.sql
@@ -1,8 +1,3 @@
--- crud operations
-
-
--- error handling
-
 --force positive values
 INSERT INTO GRADUATE (Undergraduate_GPA, Undergraduate_Degree, Program_Minor, Double_Degree, Student_ID) VALUES (-3.0, 'Physics', 'none', FALSE, 904);
 
@@ -23,13 +18,16 @@ INSERT INTO Course (Course_Code,
 
 -- joins
 
+--basic join for 3 tables
+SELECT *
+FROM Extracurricular
+JOIN Clubs ON Extracurricular.Club_ID = Clubs.Club_ID
+JOIN Jobs ON Extracurricular.Job_ID = Jobs.Job_ID;
+
 --inner join
 SELECT Student.Student_ID, Graduate.Undergraduate_Degree FROM Student
 INNER JOIN Graduate
 ON Student.Student_ID = Graduate.Student_ID;
-
---aggregate join
-
 
 --self join
 SELECT A.Student_ID AS id1, B.Student_ID AS id2, A.Province
@@ -38,6 +36,27 @@ WHERE A.Student_ID <> B.Student_ID
 AND A.Province = B.Province
 ORDER BY A.City;
 
--- pattern matching
+-- pattern matching/aggregate
+
+--summary using aggregate functions
+SELECT AVG(Overall_GPA), MIN(Overall_GPA), MAX(Overall_GPA) FROM Student;
+
+START TRANSACTION;
+
+DELETE FROM Contact_Information WHERE School_Name_2 = 'none';
+
+ROLLBACK;
+COMMIT;
 
 -- transactions
+START TRANSACTION;
+
+UPDATE Student SET Overall_GPA = 3.3 WHERE Student_ID = 006;
+INSERT INTO Student (Student_ID, First_Name, Last_Name, Gender, Pronouns, International_Student, Date_of_Admission, Overall_GPA) VALUES (011, 'Ida', 'Humphrey', 'Female', 'She/Her', TRUE, '2022-01-15', 3.6);
+DELETE FROM Student WHERE Student_ID = 007;
+SELECT * FROM Student;
+
+ROLLBACK;
+SELECT * FROM Student;
+
+COMMIT;
