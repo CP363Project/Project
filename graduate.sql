bfd97c47e94ef6dacc2769483de9d2a6eb9db3bb
affansaiyed <affanimam123@gmail.com>
2023-11-26 23:14:44 -0500

    update test scripts

--- graduate.sql
+++ graduate.sql
@@ -1,4 +1,4 @@
-INSERT INTO GRADUATE (Undergraduate_GPA, Undergraduate_Degree, Program_Minor, Double_Degree, Student_ID)VALUES (3.8, 'Mathematics', NULL, FALSE, 002);
+INSERT INTO GRADUATE (Undergraduate_GPA, Undergraduate_Degree, Program_Minor, Double_Degree, Student_ID)VALUES (3.8, 'Mathematics', "none", FALSE, 002);
 INSERT INTO GRADUATE (Undergraduate_GPA, Undergraduate_Degree, Program_Minor, Double_Degree, Student_ID)VALUES (3.9, 'Computer Science', 'Sustainability', TRUE, 005);
-INSERT INTO GRADUATE (Undergraduate_GPA, Undergraduate_Degree, Program_Minor, Double_Degree, Student_ID)VALUES (3.5, 'Engineering', NULL, TRUE, 008);
+INSERT INTO GRADUATE (Undergraduate_GPA, Undergraduate_Degree, Program_Minor, Double_Degree, Student_ID)VALUES (3.5, 'Engineering', "none", TRUE, 008);
 INSERT INTO GRADUATE (Undergraduate_GPA, Undergraduate_Degree, Program_Minor, Double_Degree, Student_ID)VALUES (3.7, 'Biology', 'Chemistry', FALSE, 009);
